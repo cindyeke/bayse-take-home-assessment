393dd4b1857e3bb214262b35ae2c99a5ac9a445b
Cindy Eke <officialcee1@gmail.com>
2026-07-01 14:31:42 +0100

    chore: add design tokens (colors, fonts) to tailwind theme

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,29 @@
+import { Onest, Inter, Archivo } from "next/font/google";
+import "./globals.css";
+
+const onest = Onest({
+  subsets: ["latin"],
+  variable: "--font-onest",
+});
+const inter = Inter({
+  subsets: ["latin"],
+  variable: "--font-inter",
+});
+const archivo = Archivo({
+  subsets: ["latin"],
+  variable: "--font-archivo",
+});
+
 export default function RootLayout({
   children,
 }: Readonly<{
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
+    <html
+      lang="en"
+      className={`${onest.variable} ${inter.variable} ${archivo.variable} h-full antialiased`}
+    >
       <body>{children}</body>
     </html>
   );
